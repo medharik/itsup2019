90f008d9528918024b38a4cbee2e77cf43deb072
unknown <med.harik@gmail.com>
2019-03-26 20:43:24 +0000

    jquery intro

---
+++ js/jquery/test.js
@@ -0,0 +1,12 @@
+function cacher(){
+// var x=document.getElementById('para');
+// x.style.display='none';
+$('#para').hide();
+}
+function montrer(){
+    // var x=document.getElementById('para');
+    // x.style.display='block';
+    $('#para').show();
+}
+
+
